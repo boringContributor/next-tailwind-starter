456885b4217eda6d5b57edb59fb346447e85bbfa
Sebastian Sauerer <sebastian.sauerer@sap.com>
2022-09-22 09:33:00 +0200

    init

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import type { NextPage } from "next";
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <nav></nav>
+      <main></main>
+
+      <footer></footer>
+    </>
+  );
+};
+
+export default Home;
